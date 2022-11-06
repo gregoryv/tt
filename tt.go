56a0faae420fbc8d74eb6cf85f8772eacf3219e9
Gregory Vincic <gregoryvse@gmail.com>
2022-11-06 18:00:39 +0100

    Refactor

--- tt.go
+++ tt.go
@@ -36,3 +36,17 @@ func NoopPub(_ context.Context, _ *mq.Publish) error   { return nil }
 type Conn interface {
 	io.ReadWriter
 }
+
+// ----------------------------------------
+
+func CheckForm(next Handler) Handler {
+	return func(ctx context.Context, p mq.Packet) error {
+		if p, ok := p.(interface{ WellFormed() error }); ok {
+			// todo mq.WellFormed must return a specific error
+			if err := p.WellFormed(); err != nil {
+				return err
+			}
+		}
+		return next(ctx, p)
+	}
+}
