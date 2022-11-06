d8386e2d003f9f0bfcc45b82dca9aff8ec1227fc
Gregory Vincic <gregoryvse@gmail.com>
2022-11-06 11:29:54 +0100

    Add func Client.Send

--- client.go
+++ client.go
@@ -1,9 +1,20 @@
 package tt
 
+import (
+	"context"
+
+	"github.com/gregoryv/mq"
+)
+
 func NewClient() *Client {
 	return &Client{}
 }
 
 type Client struct {
 	Conn
+}
+
+func (c *Client) Send(ctx context.Context, p mq.Packet) error {
+	_, err := p.WriteTo(c.Conn)
+	return err
 }
--- client_test.go
+++ client_test.go
@@ -1,7 +1,21 @@
 package tt
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/gregoryv/mq"
+)
 
 func TestClient(t *testing.T) {
-	_ = NewClient()
+	c := NewClient()
+	c.Conn = Dial()
+
+	{ // can publish
+		p := mq.NewPublish()
+		p.SetTopicName("gopher/happy")
+		p.SetPayload([]byte("yes"))
+		c.Send(context.Background(), p)
+	}
+
 }
