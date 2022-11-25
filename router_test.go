208b54abf33a78f49cae7adcf6df5f8e63321363
Gregory Vincic <gregoryvse@gmail.com>
2022-11-25 07:36:13 +0100

    Update TestRouter

--- router_test.go
+++ router_test.go
@@ -3,24 +3,32 @@ package tt
 import (
 	"context"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/gregoryv/mq"
 )
 
 func TestRouter(t *testing.T) {
+	var wg sync.WaitGroup
+	var handle = func(_ context.Context, _ *mq.Publish) error {
+		wg.Done()
+		return nil
+	}
 	routes := []*Route{
-		NewRoute("gopher/pink", NoopPub),
+		NewRoute("gopher/pink", handle),
 		NewRoute("gopher/blue", NoopPub),
-		NewRoute("#", NoopPub),
+		NewRoute("#", handle),
 	}
 	r := NewRouter(routes...)
 
+	// number of handle routes that should be triggered by below Pub
+	wg.Add(2)
 	ctx := context.Background()
 	if err := r.In(ctx, mq.Pub(0, "gopher/pink", "hi")); err != nil {
 		t.Error(err)
 	}
-
+	wg.Wait()
 	if v := r.String(); !strings.Contains(v, "3 routes") {
 		t.Error(v)
 	}
