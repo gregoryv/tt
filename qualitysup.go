515548b6cff7bdfa823d0ca2f3f8b19786ea0af7
Gregory Vincic <gregoryvse@gmail.com>
2022-11-26 17:15:06 +0100

    Reference spec

--- qualitysup.go
+++ qualitysup.go
@@ -22,6 +22,8 @@ func (s *QualitySupport) In(next Handler) Handler {
 	return func(ctx context.Context, p mq.Packet) error {
 		switch p := p.(type) {
 		case *mq.Publish:
+			// Disconnect any attempts to publish exceeding qos.
+			// Specified in section 3.3.1.2 QoS
 			if p.QoS() > s.max {
 				d := mq.NewDisconnect()
 				d.SetReasonCode(mq.QoSNotSupported)
