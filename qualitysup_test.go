ca5f473e5f8b1670ba736667100d35302e8eff69
Gregory Vincic <gregoryvse@gmail.com>
2022-11-26 17:23:56 +0100

    Test QualitySupport

--- qualitysup_test.go
+++ qualitysup_test.go
@@ -8,7 +8,36 @@ import (
 	"github.com/gregoryv/mq"
 )
 
-func TestQualitySupport_In(t *testing.T) {
+func TestQualitySupport(t *testing.T) {
 	f := NewQualitySupport(Send(ioutil.Discard))
-	f.In(NoopHandler)(context.Background(), mq.Pub(0, "a/b", "hi"))
+	ctx := context.Background()
+
+	{ // ok pub
+		p := mq.Pub(0, "a/b", "hi")
+		if err := f.In(NoopHandler)(ctx, p); err != nil {
+			t.Fatal(err)
+		}
+	}
+	{ // pub with unsupported qos
+		p := mq.Pub(1, "a/b", "hi")
+		called := NewCalled()
+		if err := f.In(called.Handler)(ctx, p); err != nil {
+			t.Fatal(err)
+		}
+		select {
+		case <-called.Done():
+			t.Error("was called")
+		default:
+		}
+	}
+	{ // outgoing ack
+		p := mq.NewConnAck()
+		if err := f.Out(NoopHandler)(ctx, p); err != nil {
+			t.Fatal(err)
+		}
+		if v := p.MaxQoS(); v != 0 {
+			t.Errorf("max qos set to %v", v)
+		}
+	}
+
 }
