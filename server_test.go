5f0084369ffff6e72d4cb2d1c05d19b4d1ae72a1
Gregory Vincic <gregoryvse@gmail.com>
2023-02-05 19:01:54 +0100

    Refactor tests

--- server_test.go
+++ server_test.go
@@ -1,7 +1,6 @@
 package tt
 
 import (
-	"context"
 	"reflect"
 	"testing"
 	"time"
@@ -10,14 +9,14 @@ import (
 )
 
 func TestServer_AddConnection(t *testing.T) {
+	// Server keeps statistics of active and total connections
 	s := NewServer()
 	conn := dial(s)
-	<-time.After(10 * time.Millisecond)
+	<-time.After(5 * time.Millisecond)
 	before := s.Stat()
 
-	mq.NewDisconnect().WriteTo(conn)
-
-	<-time.After(10 * time.Millisecond)
+	conn.Close()
+	<-time.After(5 * time.Millisecond)
 	after := s.Stat()
 
 	if reflect.DeepEqual(before, after) {
@@ -30,20 +29,16 @@ func TestServer_AssignsID(t *testing.T) {
 	// assign one in the returning ConnAck.
 	s := NewServer()
 	conn := dial(s)
-	{
-		p := mq.NewConnect()
-		p.WriteTo(conn)
-	}
-	{
-		p, _ := mq.ReadPacket(conn)
-		switch p := p.(type) {
-		case *mq.ConnAck:
-			if p.AssignedClientID() == "" {
-				t.Error("missing assigned client id")
-			}
-		default:
-			t.Fatal(p)
+	mq.NewConnect().WriteTo(conn)
+
+	p, _ := mq.ReadPacket(conn)
+	switch p := p.(type) {
+	case *mq.ConnAck:
+		if p.AssignedClientID() == "" {
+			t.Error("missing assigned client id")
 		}
+	default:
+		t.Fatal(p)
 	}
 }
 
@@ -55,7 +50,7 @@ func TestServer_DisconnectOnMalformed(t *testing.T) {
 	{
 		p := mq.NewConnect()
 		p.WriteTo(conn)
-		_, _ = mq.ReadPacket(conn)
+		_, _ = mq.ReadPacket(conn) // ignore ack
 	}
 	{
 		p := mq.NewPublish()
@@ -73,49 +68,7 @@ func TestServer_DisconnectOnMalformed(t *testing.T) {
 			t.Fatal(p)
 		}
 	}
-	<-time.After(10 * time.Millisecond)
-	if s := s.Stat(); s.ConnActive != 0 {
-		t.Error(s)
-	}
-}
-
-func xTestServer_createHandlers(t *testing.T) {
-	conn := NewMemConn().Server()
-	in, _ := NewServer().createHandlers(conn)
-	{ // accepted packets
-		packets := []mq.Packet{
-			mq.NewConnect(),
-			mq.Pub(0, "a/b", ""),
-			func() mq.Packet {
-				p := mq.Pub(1, "a/b", "")
-				p.SetPacketID(1)
-				return p
-			}(),
-			// rejected with a disconnect, but handled properly
-			func() mq.Packet {
-				p := mq.Pub(2, "a/b", "")
-				p.SetPacketID(11)
-				return p
-			}(),
-		}
-		ctx := context.Background()
-		for _, p := range packets {
-			if err := in(ctx, p); err != nil {
-				t.Fatal(err)
-			}
-		}
-	}
-	{ // denied packets
-		packets := []mq.Packet{
-			mq.Pub(0, "", ""), // malformed, missing topic
-			mq.NewSubscribe(),
-		}
-		ctx := context.Background()
-		for _, p := range packets {
-			if err := in(ctx, p); err == nil {
-				t.Logf("on %v", p)
-				t.Errorf("expect incoming handler to fail")
-			}
-		}
+	if _, err := mq.NewPublish().WriteTo(conn); err == nil {
+		t.Error("still connected")
 	}
 }
