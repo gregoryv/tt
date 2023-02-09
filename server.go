ae746dd0c2957d6a3f74b860e5ac0f4b385020f0
Gregory Vincic <gregoryvse@gmail.com>
2023-02-09 20:32:03 +0100

    Merge server.go with cidmaker.go
    Grouping server related types

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gregoryv/mq"
 )
 
@@ -123,5 +124,29 @@ func (d *Disconnector) Out(next Handler) Handler {
 			d.conn.Close()
 		}
 		return err
+	}
+}
+
+func NewClientIDMaker(transmit Handler) *ClientIDMaker {
+	return &ClientIDMaker{
+		transmit: transmit,
+	}
+}
+
+type ClientIDMaker struct {
+	transmit Handler
+}
+
+func (c *ClientIDMaker) In(next Handler) Handler {
+	return func(ctx context.Context, p mq.Packet) error {
+		switch p := p.(type) {
+		case *mq.Connect:
+			a := mq.NewConnAck()
+			if id := p.ClientID(); id == "" {
+				a.SetAssignedClientID(uuid.NewString())
+			}
+			return c.transmit(ctx, a)
+		}
+		return next(ctx, p)
 	}
 }
