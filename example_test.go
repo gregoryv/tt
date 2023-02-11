963ccb0b61f1fdf572cbf7337deb11c93bd8759a
Gregory Vincic <gregoryvse@gmail.com>
2023-02-11 09:14:01 +0100

    Refactor Example_client

--- example_test.go
+++ example_test.go
@@ -13,40 +13,40 @@ import (
 // Example shows a simple client for connect, publish a QoS 0 and
 // disconnect.
 func Example_client() {
-	// standin for a network connection
 	conn, _ := testnet.Dial("tcp", "someserver:1234")
-	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+
+	// transmit handler for synced packet writes
 	transmit := tt.Send(conn)
-	handler := func(ctx context.Context, p mq.Packet) error {
-		switch p := p.(type) {
-		case *mq.ConnAck:
 
-			switch p.ReasonCode() {
-			case mq.Success: // we've connected successfully
-				// publish a message
-				m := mq.Pub(0, "gopher/happy", "yes")
-				if err := transmit(ctx, m); err != nil {
-					log.Print(err)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond)
+	receiver := tt.NewReceiver(conn,
+		func(ctx context.Context, p mq.Packet) error {
+			switch p := p.(type) {
+			case *mq.ConnAck:
+
+				switch p.ReasonCode() {
+				case mq.Success: // we've connected successfully
+					// publish a message
+					m := mq.Pub(0, "gopher/happy", "yes")
+					if err := transmit(ctx, m); err != nil {
+						log.Print(err)
+					}
+					// disconnect
+					defer cancel()
+					return transmit(ctx, mq.NewDisconnect())
 				}
-				// disconnect
-				defer cancel()
-				return transmit(ctx, mq.NewDisconnect())
 			}
-
-			// handle refused connection
 			return nil
-		}
-		return nil
-	}
+		},
+	)
 
-	// connect
+	// initiate connect sequence
 	if err := transmit(ctx, mq.NewConnect()); err != nil {
 		log.Fatal(err)
 	}
 
 	// start receiving packets
-	receive := tt.NewReceiver(conn, handler)
-	if err := receive.Run(ctx); err != nil {
+	if err := receiver.Run(ctx); err != nil {
 		log.Print(err)
 	}
 }
