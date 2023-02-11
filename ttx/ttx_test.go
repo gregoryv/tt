dd1bd9c54bcaaf880a8e8ba842857743f9be6d52
Gregory Vincic <gregoryvse@gmail.com>
2023-02-11 10:46:37 +0100

    Add TestClosedConn

--- ttx/ttx_test.go
+++ ttx/ttx_test.go
@@ -20,3 +20,14 @@ func TestCalled(t *testing.T) {
 	go c.Handler(nil, nil)
 	<- c.Done()
 }
+
+func TestClosedConn(t *testing.T) {
+	var c ClosedConn
+	buf := make([]byte, 10)
+	if _, err := c.Read(buf); err == nil {
+		t.Fatal("Read should faild")
+	}
+	if _, err := c.Write(buf); err == nil {
+		t.Fatal("Read should faild")
+	}	
+}
