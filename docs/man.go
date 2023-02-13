4708f6e456f06946ff62007d16d4863f4240b075
Gregory Vincic <gregoryvse@gmail.com>
2023-02-13 07:45:01 +0100

    Add command sections to manual

--- docs/man.go
+++ docs/man.go
@@ -10,34 +10,64 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/gregoryv/web"
 	. "github.com/gregoryv/web"
+	"github.com/gregoryv/web/theme"
+	"github.com/gregoryv/web/toc"
 )
 
 func main() {
 	log.SetFlags(log.Flags() | log.Lshortfile)
 
+	nav := Nav()
+
 	doc := Article(
 		H1("tt - manual"),
+		nav,
 
-		Section(
-			H2("-h, --help"),
-			Pre(
-				must(exec.Command("tt", "-hs")),
-			),
+		H2("Options"),
+
+		H3("-h, --help"),
+		Pre(
+			must(exec.Command("tt", "-h")),
 		),
 
-		Section(H2("Design"),
-			NewDesignDiagram().Inline(),
-			NewConnectCleanStart().Inline(),
-		),
+		H2("Commands"),
+
+		H3("pub"),
+
+		H4("Publish QoS 0"),
+
+		NewConnectCleanStart().Inline(),
+
+		H3("sub"),
+
+		H3("srv"),
 	)
-
+	toc.MakeTOC(nav, doc, "h2", "h3", "h4")
 	// compose manual page
 	NewFile("man.html",
 		Html(
+			Head(
+				Style(
+					theme.GoldenSpace(),
+					theme.GoishColors(),
+					manTheme(),
+				),
+			),
 			Body(doc),
 		),
 	).SaveTo(".")
+}
+
+func manTheme() *web.CSS {
+	css := web.NewCSS()
+	css.Style("nav ul",
+		"list-style-type: none",
+	)
+	css.Style("li.h3", "margin-left: 2em")
+	css.Style("li.h4", "margin-left: 4em")
+	return css
 }
 
 func must(cmd *exec.Cmd) string {
