dbc924ecddddbf1ee229dec1735c38ea5ce28aa3
Gregory Vincic <gregoryvse@gmail.com>
2023-02-16 22:09:07 +0100

    Add func docs.Manual

--- docs/man.go
+++ docs/man.go
@@ -22,59 +22,6 @@ import (
 func main() {
 	log.SetFlags(log.Flags() | log.Lshortfile)
 
-	nav := Nav()
-
-	doc := Article(
-		H1("tt - manual"),
-		time.Now().Format("2006-01-02 15:04:05"),
-		Br(),
-		nav,
-
-		H2("Options"),
-
-		H3("-h, --help"),
-		Pre(
-			must(exec.Command("tt", "-h")),
-		),
-
-		H2("Commands"),
-
-		H3("pub"),
-
-		H4("Publish QoS 0"),
-
-		// diagram showing package flow, maybe just dump the output
-		func() *design.SequenceDiagram {
-			var (
-				d = design.NewSequenceDiagram()
-				c = d.Add("client")
-				s = d.Add("server")
-			)
-			d.ColWidth = 300
-			{
-				p := mq.NewConnect()
-				p.SetCleanStart(true)
-				d.Link(c, s, p.String())
-			}
-			{
-				p := mq.NewConnAck()
-				p.SetTopicAliasMax(10) // default in mosquitto
-				d.Link(s, c, p.String())
-			}
-			d.Link(c, s, mq.Pub(0, "gopher/pink", "hug").String())
-			d.Link(c, s, mq.NewDisconnect().String())
-
-			d.SetCaption("Client connects with clean start flag set to true")
-			return d
-		}().Inline(),
-
-		Pre(must(exec.Command("tt"))),
-
-		H3("sub"),
-
-		H3("srv"),
-	)
-	toc.MakeTOC(nav, doc, "h2", "h3", "h4")
 	// compose manual page
 	NewFile("man.html",
 		Html(
@@ -85,13 +32,77 @@ func main() {
 					manTheme(),
 				),
 			),
-			Body(doc),
+			Body(Manual()),
 		),
 	).SaveTo(".")
 }
 
+func Manual() *Element {
+	nav := Nav("Table of contents")
+	doc := Wrap(
+		Header(
+			time.Now().Format("2006-01-02 15:04:05"),
+		),
+
+		Article(
+			H1("Telemetry Transfer (tt) - manual"),
+
+			Br(),
+			nav,
+
+			H2("Options"),
+
+			H3("-h, --help"),
+			Pre(
+				must(exec.Command("tt", "-h")),
+			),
+
+			H2("Commands"),
+
+			H3("pub"),
+
+			H4("Publish QoS 0"),
+
+			// diagram showing package flow, maybe just dump the output
+			func() *design.SequenceDiagram {
+				var (
+					d = design.NewSequenceDiagram()
+					c = d.Add("client")
+					s = d.Add("server")
+				)
+				d.ColWidth = 300
+				{
+					p := mq.NewConnect()
+					p.SetCleanStart(true)
+					d.Link(c, s, p.String())
+				}
+				{
+					p := mq.NewConnAck()
+					p.SetTopicAliasMax(10) // default in mosquitto
+					d.Link(s, c, p.String())
+				}
+				d.Link(c, s, mq.Pub(0, "gopher/pink", "hug").String())
+				d.Link(c, s, mq.NewDisconnect().String())
+
+				d.SetCaption("Client connects with clean start flag set to true")
+				return d
+			}().Inline(),
+
+			Pre(must(exec.Command("tt"))),
+
+			H3("sub"),
+
+			H3("srv"),
+		))
+	toc.MakeTOC(nav, doc, "h2", "h3", "h4")
+	return doc
+}
+
 func manTheme() *web.CSS {
 	css := web.NewCSS()
+	css.Style("header",
+		"text-align: right",
+	)
 	css.Style("nav ul",
 		"list-style-type: none",
 	)
