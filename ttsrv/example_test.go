7c7112505746d3c7ac381cc7b84c8b9737afef24
Gregory Vincic <gregoryvse@gmail.com>
2023-02-19 17:17:18 +0100

    Simplify Example_server

--- ttsrv/example_test.go
+++ ttsrv/example_test.go
@@ -2,9 +2,7 @@ package ttsrv_test
 
 import (
 	"context"
-	"log"
-	"net"
-	"time"
+	"fmt"
 
 	"github.com/gregoryv/tt/ttsrv"
 )
@@ -12,16 +10,11 @@ import (
 // Example shows how to run the provided server.
 func Example_server() {
 	s := ttsrv.NewServer()
-	b, _ := ttsrv.NewBindConf("tcp://:", "20s")
-	s.Binds = append(s.Binds, b)
-
 	go s.Run(context.Background())
 
-	<-time.After(time.Millisecond)
-	conn, err := net.Dial(b.URL.Scheme, b.URL.Host)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-	// use it in your client
+	b := s.Binds[0] // by default there is one
+	fmt.Println(b.URL)
+
+	// output:
+	// tcp://localhost:
 }
