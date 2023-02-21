f90cba2ae7cde83b848600b615f89ed297a01b35
Gregory Vincic <gregoryvse@gmail.com>
2023-02-21 07:57:06 +0100

    Remove type Logger

--- ttsrv/server.go
+++ ttsrv/server.go
@@ -2,7 +2,9 @@
 package ttsrv
 
 import (
+	"bytes"
 	"context"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
@@ -259,3 +261,18 @@ type Connection interface {
 	io.ReadWriteCloser
 	RemoteAddr() net.Addr
 }
+
+func dumpPacket(p mq.Packet) string {
+	var buf bytes.Buffer
+	p.WriteTo(&buf)
+	return hex.Dump(buf.Bytes())
+}
+
+func trimID(s string, width uint) string {
+	if v := uint(len(s)); v > width {
+		return prefixStr + s[v-width:]
+	}
+	return s
+}
+
+const prefixStr = "~"
