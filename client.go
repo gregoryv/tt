9f292fa6fc206fb143e4c4da6840e3f6e14be0fb
Gregory Vincic <gregoryvse@gmail.com>
2023-02-22 08:10:54 +0100

    Dial server in Client.Run

--- client.go
+++ client.go
@@ -3,7 +3,10 @@ package tt
 import (
 	"context"
 	"fmt"
+	"net"
 	"net/url"
+
+	"github.com/gregoryv/mq"
 )
 
 func NewClient() *Client {
@@ -16,8 +19,31 @@ type Client struct {
 
 	// Server to connect to
 	Server *url.URL
+
+	ClientID string
+
+	Debug bool
 }
 
 func (c *Client) Run(ctx context.Context) error {
-	return fmt.Errorf(": todo")
+	// use middlewares and build your in/out queues with desired
+	// features
+	log := NewLogger()
+	log.SetLogPrefix(c.ClientID)
+	log.SetDebug(c.Debug)
+
+	// dial server
+	host := c.Server.String()
+	log.Print("dial tcp://", host)
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		return err
+	}
+
+	_ = conn // wip use it
+	return fmt.Errorf("Client.Run: todo")
+}
+
+func (c *Client) Send(ctx context.Context, p mq.Packet) error {
+	return fmt.Errorf("Client.Send: todo")
 }
