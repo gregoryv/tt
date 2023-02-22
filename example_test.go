8c5edf5413e7a96b08d6fed16e549446faac2e06
Gregory Vincic <gregoryvse@gmail.com>
2023-02-22 20:06:47 +0100

    Remove Example_publishQoS1

--- example_test.go
+++ example_test.go
@@ -3,7 +3,6 @@ package tt_test
 import (
 	"context"
 	"log"
-	"net"
 	"time"
 
 	"github.com/gregoryv/mq"
@@ -44,34 +43,4 @@ func Example_client() {
 	if err := client.Run(ctx, app); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func Example_publishQoS1() {
-	// open
-	conn, err := net.Dial("tcp", "localhost:1883")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := mq.NewConnect().WriteTo(conn); err != nil {
-		log.Fatal(err)
-	}
-	// wait for connack
-	if _, err := mq.ReadPacket(conn); err != nil {
-		log.Fatal(err)
-	}
-
-	p := mq.Pub(1, "a/b", "hello")
-	p.SetPacketID(1)
-	if _, err := p.WriteTo(conn); err != nil {
-		log.Fatal(err)
-	}
-
-	if _, err := mq.ReadPacket(conn); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := mq.NewDisconnect().WriteTo(conn); err != nil {
-		log.Fatal(err)
-	}
-	// close
-	conn.Close()
 }
