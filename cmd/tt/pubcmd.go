9b8321601e67f4bc35439d96b5f1efbe78197469
Gregory Vincic <gregoryvse@gmail.com>
2023-02-23 05:39:11 +0100

    Use pubcmd timeout

--- cmd/tt/pubcmd.go
+++ cmd/tt/pubcmd.go
@@ -32,13 +32,16 @@ func (c *PubCmd) ExtraOptions(cli *cmdline.Parser) {
 	c.topic = cli.Option("-t, --topic-name").String("gopher/pink")
 	c.payload = cli.Option("-p, --payload").String("hug")
 	c.qos = cli.Option("-q, --qos").Uint8(0)
-	c.timeout = cli.Option("    --timeout").Duration("1s") // wip global ?
+	c.timeout = cli.Option("    --timeout").Duration("1s")
 	c.username = cli.Option("-u, --username").String("")
 	c.password = cli.Option("-p, --password").String("")
 }
 
 func (c *PubCmd) Run(ctx context.Context) error {
 	ctx, cancel := context.WithCancel(ctx)
+	if c.timeout > 0 {
+		ctx, cancel = context.WithTimeout(ctx, c.timeout)
+	}
 
 	client := &tt.Client{
 		Server:      c.server,
