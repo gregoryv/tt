2644eed8b3ab3e7f162ef3ce2ca764f25aeed4f6
Gregory Vincic <gregoryvse@gmail.com>
2023-02-25 17:06:02 +0100

    Update example

--- example_test.go
+++ example_test.go
@@ -12,30 +12,31 @@ import (
 // disconnect.
 func Example_client() {
 	client := &tt.Client{
-		Server: "tcp://localhost:1883",
-
-		OnPacket: func(ctx context.Context, c *tt.Client, p mq.Packet) {
-			switch p := p.(type) {
-			case *mq.ConnAck:
-
-				switch p.ReasonCode() {
-				case mq.Success:
-					p := mq.Pub(0, "gopher/happy", "yes")
-					_ = c.Send(ctx, p)
-				}
-
-			}
-		},
-
-		OnEvent: func(ctx context.Context, c *tt.Client, e tt.Event) {
-			switch e {
-			case tt.EventClientUp:
-				_ = c.Send(ctx, mq.NewConnect())
-			}
-		},
+		Server:   "tcp://localhost:1883",
+		OnPacket: onPacket,
+		OnEvent:  onEvent,
 	}
 
 	if err := client.Run(context.Background()); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func onPacket(ctx context.Context, c *tt.Client, p mq.Packet) {
+	switch p := p.(type) {
+	case *mq.ConnAck:
+
+		switch p.ReasonCode() {
+		case mq.Success:
+			p := mq.Pub(0, "gopher/happy", "yes")
+			_ = c.Send(ctx, p)
+		}
+	}
+}
+
+func onEvent(ctx context.Context, c *tt.Client, e tt.Event) {
+	switch e {
+	case tt.EventClientUp:
+		_ = c.Send(ctx, mq.NewConnect())
 	}
 }
