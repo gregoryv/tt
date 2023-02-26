33d8fa8b0c332dcd800295f338c9db5fe160460f
Gregory Vincic <gregoryvse@gmail.com>
2023-02-26 09:23:06 +0100

    Document package event

--- event/event.go
+++ event/event.go
@@ -1,5 +1,14 @@
+// Package event provides client and server event types.
+//
+// In addition to packet types event types are used to inform the
+// application of what is happening inside a [tt.Client] or
+// [tt.Server].
+//
+// [tt.Client]: https://pkg.go.dev/github.com/gregoryv/tt#Client
+// [tt.Server]: https://pkg.go.dev/github.com/gregoryv/tt#Server
 package event
 
+// ClientUp indicates client is ready for sending packets.
 type ClientUp uint8
 type ClientConnect uint8
 type ClientConnectFail string
