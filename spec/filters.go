78d49e19d34d4ad45cc25cdb8397891cc7abbcfd
Gregory Vincic <gregoryvse@gmail.com>
2023-12-31 14:20:38 +0100

    Add func spec.VerifyFilterFormat

--- spec/filters.go
+++ spec/filters.go
@@ -1,9 +1,64 @@
+// Package spec implements implementation verifiers against the MQTTv5 specification
+//
+// [4.7 Topic names and filters]
+//
+// [4.7 Topic names and filters]: https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901241
 package spec
 
 import (
 	"errors"
 	"fmt"
 )
+
+// VerifyFilterFormat verifies topic filter implementation against the
+// given rules. If no rules are given, default set of rules are used.
+func VerifyFilterFormat(fn func(filter string) bool, rules ...RuleFilterFormat) error {
+	var all []error
+	if len(rules) == 0 {
+		rules = RulesFilterFormat
+	}
+	for _, rule := range rules {
+		if err := rule.Verify(fn); err != nil {
+			all = append(all, err)
+		}
+	}
+	return errors.Join(all...)
+}
+
+var RulesFilterFormat = []RuleFilterFormat{
+	{true, "#"},
+	{true, "/#"},
+	{true, "a/b/#"},
+	{true, "+"},
+	{true, "+/"},
+	{true, "+/+"},
+	{true, "/+/+"},
+	{true, "a/+/c/+"},
+
+	{false, " #"},
+	{false, "#/"},
+	{false, " +"},
+	{false, "++/+"},
+	{false, "/a+"},
+}
+
+type RuleFilterFormat struct {
+	Exp    bool
+	Filter string
+}
+
+func (r *RuleFilterFormat) Verify(fn func(string) bool) error {
+	got := fn(r.Filter)
+	if r.Exp && !got {
+		return fmt.Errorf("%s should be acceptable format", r.Filter)
+	}
+	if !r.Exp && got {
+		return fmt.Errorf("%s should NOT be acceptable format", r.Filter)
+	}
+	return nil
+}
+
+// ----------------------------------------
 
 // VerifyFilterMatching verifies match func against the given
 // rules. If no rules are given, default set of rules is used.
--- spec/filters_test.go
+++ spec/filters_test.go
@@ -2,6 +2,17 @@ package spec
 
 import "testing"
 
+func TestVerify_brokenFormater(t *testing.T) {
+	impl := func(f string) bool { return false }
+	if err := VerifyFilterFormat(impl); err == nil {
+		t.Error("should return error")
+	}
+	impl = func(f string) bool { return true }
+	if err := VerifyFilterFormat(impl); err == nil {
+		t.Error("should return error")
+	}
+}
+
 func TestVerify_brokenMatcher(t *testing.T) {
 	impl := func(f, n string) bool { return false }
 	if err := VerifyFilterMatching(impl); err == nil {
